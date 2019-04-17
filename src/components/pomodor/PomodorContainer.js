bdba78c7fcb39b18885e5ed2e1d507e16cb8a9e3
Konstantin <barrbozzopower@gmail.com>
2019-04-17 12:25:28 +0400

    Started Pomodor components realization

---
+++ src/components/pomodor/PomodorContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PomodorContainer extends Component {
+  render() {
+    return (
+      <div>
+        pmodor
+      </div>
+    );
+  }
+}
+
+export default PomodorContainer;
