a627b8d3ccf3c57ec439c8bbd46c9d462e4bd3f1
Konstantin <barrbozzopower@gmail.com>
2019-04-19 15:20:24 +0400

    Feat: Connect store to localStorage by subscribe

--- src/store.js
+++ src/store.js
@@ -1,12 +1,20 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import reduxThunk from "redux-thunk";
 import logger from 'redux-logger';
+import throttle from 'lodash/throttle';
 import rootReducer from "./reducers/index";
+import {loadState, saveState} from './utils/localStorage';
 
+const persistedState = loadState();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
+  persistedState,
   composeEnhancers(applyMiddleware(reduxThunk, logger))
 );
+
+store.subscribe(throttle(() => {
+  saveState(store.getState());
+}, 1000));
 
 export default store;
