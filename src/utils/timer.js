f901a1d571fcd956fd9a3d980e626c7555885a3f
Konstantin <barrbozzopower@gmail.com>
2019-04-19 14:45:51 +0400

    Feat: Get current remain for pomodor timer

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -4,3 +4,8 @@ export const convertToDisplay = (ms = 0) => {
   const seconds = showWith0(Math.floor((ms / 1000) % 60));
   return `${minutes}:${seconds}`;
 };
+
+export const calculateInitialRemain = ({remains, started, state, expire}) => {
+  const now = new Date().getTime();
+  return (state === 'started') ? (now > expire ? 0 : remains - (now - started)) : remains;
+};
