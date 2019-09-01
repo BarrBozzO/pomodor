301953be569c95a40d56d6fe20e1b3bbd2511f01
Konstantin <barr.develop@gmail.com>
2019-09-01 16:18:29 +0400

    add constant values

---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const periodTypes = {
+  pomodoro: "Pomodor",
+  long: "Отдых",
+  short: "Перерыв"
+};
+
+export const defaultSettings = {
+  long: 900000, // 15 * 60 * 1000
+  short: 300000, // 5 * 60 * 1000
+  pomodoro: 1500000, // 15 * 60 * 1000
+  notifyAllowed: false
+};
--- src/store/reducers/pomodorSettings.js
+++ src/store/reducers/pomodorSettings.js
@@ -1,20 +1,16 @@
-import {
-  CHANGE_SETTINGS
-} from '../actions/types';
+import { CHANGE_SETTINGS } from "../actions/types";
+import { defaultSettings } from "../../constants";
 
 const initialState = {
-    long: 900000, // 15 * 60 * 1000
-    short: 300000, // 5 * 60 * 1000
-    pomodoro: 1500000, // 15 * 60 * 1000
-    notifyAllowed: false
+  ...defaultSettings
 };
 
 const pomodorSettingsReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_SETTINGS:
       return {
-          ...state,
-          ...action.payload
+        ...state,
+        ...action.payload
       };
     default:
       return state;
